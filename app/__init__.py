5dc38611ec295875e2c07d531c9050bccbd2ba0d
Kenneth Muchiri <kenmucciz8@gmail.com>
2019-07-01 06:44:28 +0300

    intialize the SQLAlchemy extension in the create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_state):
     app = Flask(__name__)
@@ -13,6 +14,8 @@ def create_app(config_state):
 
 
     bootstrap.init_app(app)
+    db.init_app(app)
+
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
