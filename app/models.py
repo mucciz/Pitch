191fd6b005c422db4ef3fd906a0cb43d4dbbb91b
Kenneth Muchiri <kenmucciz8@gmail.com>
2019-07-02 11:33:53 +0300

    add the date posted column

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(20, unique=True, nullable=False))
+    username = db.Column(db.String(20), unique=True, nullable=False)
     image_file = db.Column(db.String(20),nullable=False, default='default.jpg')
     hash_pass = db.Column(db.String(255),nullable=False)
 
@@ -14,8 +14,9 @@ class User(db.Model):
 class Pitch(db.Model):
     __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key = True)
-    pitch_content = db.Column(db.String())
+    pitch_title = db.Column(db.String(100),nullable=False)
     pitch_category = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime)
 
     def __repr__(self):
         return f'User {self.username}'
