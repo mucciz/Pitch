03aff0185f6c6bc2711ba6880b99f59bf9f4373c
Kenneth Muchiri <kenmucciz8@gmail.com>
2019-07-02 11:16:25 +0300

    add the user email column in the db

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(20, unique=True, nullable=False))
+    hash_pass = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    pitch_content = db.Column(db.String())
+    pitch_category = db.Column(db.String(255))
